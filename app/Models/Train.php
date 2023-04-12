46d1854c0501c228dbe1a275f6c6f1b00da7647a
Francesco Punta <puntafrancesco@gmail.com>
2023-04-12 18:15:37 +0200

    Create model train.php

---
+++ app/Models/Train.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Train extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'company',
+        'departure_station',
+        'arrival_station',
+        'departure_time',
+        'arrival_time',
+        'train_code',
+        'number_of_cars',
+        'on_time',
+        'canceled',
+    ];
+
+    protected $casts = [
+        'departure_time' => 'datetime',
+        'arrival_time' => 'datetime',
+    ];
+}
